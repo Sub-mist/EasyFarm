f58d736dab27c8fef79300e4f9290c3ac9157bbc
Kuldeep-Dilliwar <kuldeepdilliwar@gmail.com>
2024-10-14 18:52:34 +0530

    added new script 'npm run idev' which let's you access the local server over the internet if program finds the public ipv6 address of your mechine, the changed program will automatically copy the public routable url for local server

--- src/server.js
+++ src/server.js
@@ -24,13 +24,9 @@ app.set("views", templates_path);
 hbs.registerPartials(parttials_path);
 
 app.use(express.static(static_path));
-
 app.use(express.json());
-
 app.use(cookieParser());
-
 app.use(express.urlencoded({ extended: false }));
-
 
 app.use(session({
     secret: process.env.SESSION_SECRET || 'your-session-secret',
@@ -41,25 +37,34 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(router);
 
 app.listen(PORT, async () => {
     console.log("\n\n\nserver is running at :");
-    console.log(`http://localhost:${PORT}/`);
-    const publicipv6 = await getPublicIPv6();
-    console.log(`http://[${publicipv6}]:${PORT}/`);
-    openWebApp();
-    
+    if(process.env.IPV6_MODE === 'true') {
+        const publicIPv6 = await getPublicIPv6();
+        if(publicIPv6) {
+            const url = `http://[${publicIPv6}]:${PORT}/`;
+            console.log(url);
+            openWebApp(url);
+            const clipboardyModule = await import('clipboardy');
+            const clipboardy = clipboardyModule.default;
+            clipboardy.writeSync(url);
+            console.log("URL copied to clipboard");
+        } else {
+            console.log("Could not retrive IPv6, falling back to localhost");
+            openWebApp(`http://localhost:${PORT}/`);
+        } 
+    } else {
+        const url = `http://localhost:${PORT}/`
+        console.log(url);
+        openWebApp(url);
+    }
 });
 
-const openWebApp = async (publicipv6) => {
+const openWebApp = async (url) => {
     const open = await import('open');
-    if (publicipv6) {
-        open.default(`http://[${publicipv6}]:${PORT}/`);
-    } else {
-        open.default(`http://localhost:${PORT}`);
-    }
+    open.default(url);
 }
 
 const getPublicIPv6 = async () => {
@@ -69,5 +74,6 @@ const getPublicIPv6 = async () => {
         return ipv6;
     } catch (error) {
         console.error('Error fetching the IPv6 address:', error);
+        return null;
     }
 }
